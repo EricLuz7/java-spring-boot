1f490714abee9713ae2b228a3b7fa58279479c54
Eric Lodi Luz <eric_lodiluz@hotmail.com>
2023-10-09 21:36:54 -0300

    Create Pessoa.java

---
+++ AprendendoSpringBoot/src/Pessoa.java
@@ -0,0 +1,22 @@
+public class Pessoa {
+    public int nome;
+    public int idade;
+    public int getNome() {
+        return nome;
+    }
+
+    public void setNome(int nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+
+
+}
