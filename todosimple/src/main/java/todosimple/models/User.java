6607104d1a9ed2655af3aab016d4c20864ad02ce
Eric Lodi Luz <eric_lodiluz@hotmail.com>
2023-10-13 00:46:01 -0300

    created user controller

--- todosimple/src/main/java/todosimple/models/User.java
+++ todosimple/src/main/java/todosimple/models/User.java
@@ -1,5 +1,6 @@
 package todosimple.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jdk.jfr.Enabled;
 
@@ -77,6 +78,7 @@ public class User {
         this.password = password;
     }
 
+    @JsonIgnore
     public List<Task> getTasks() {
         return tasks;
     }
